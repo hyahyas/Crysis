bc2f03b7a2ce120e7766b333948a452e70d063a0
MuhammadAhsan-2 <ahsanali@codexnow.com>
2023-09-21 12:28:41 +0500

    restructured project and changed naming conventions

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,9 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use(require("./routes/userRoutes"));
+app.use(require("./src/routes/user.routes"));
 
-require("./db/conn");
+require("./src/db/conn");
  
 app.listen(port, async () => {
   console.log(`Server is running on port: ${port}`);
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { body, validationResult } = require("express-validator");
-const User = require("../models/userModel");
+const User = require("../models/user.model");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
  
