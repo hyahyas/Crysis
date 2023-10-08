ea20127b96d53879761130d9e9d9c1a69acd8fe7
Hassan <hassanyahyasheikh@gmail.com>
2023-10-08 23:53:20 +0500

    Api updates

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,6 @@ app.use(require("./src/routes/user.routes"));
 app.use(require("./src/routes/announcement.routes"))
 app.use(require("./src/routes/server.routes"))
 app.use(require("./src/routes/tickets.routes"))
-app.use(require("./src/routes/restraunts.routes"))
 require("./src/db/conn");
  
 app.listen(port, async () => {
