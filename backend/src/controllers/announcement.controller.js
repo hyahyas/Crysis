9583b6b4963a0109bcc960b0b100d35f54701492
Hassan <hassanyahyasheikh@gmail.com>
2023-11-13 17:25:35 +0500

    additon/removal of member and user control on announcement

--- backend/src/controllers/announcement.controller.js
+++ backend/src/controllers/announcement.controller.js
@@ -1,31 +1,103 @@
 const Announcement = require("../models/announcement.model");
 const jwt = require("jsonwebtoken");
+const { checkUserInServerAndAdmin } = require("../middleware/middleware");
+const {Membership} = require("../models/server.model")
 
 const logEndPoint = (type, url) => {
     console.log(new Date().toLocaleString(), "--->", type, " ", url);
 };
 
+
 // Create
 exports.createAnnouncement = async (req, res) => {
     logEndPoint("POST", "/announcements");
-
+    //check if user is member of server
     try {
-        // bearer token in header
         const token = req.headers["authorization"].split(" ")[1];
-        const user = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-        const { title, description } = req.body;
-        const newAnnouncement = new Announcement({
-            title,
-            description,
-            createdBy: user.id,
-            server: req.params.serverId,
+        if (!token) {
+            return res.status(401).json({ error: "You must be logged in" });
+        }
+
+        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        const userId = decoded.id;
+        const serverId = req.body.serverId; // Assuming serverId is in the request parameters
+
+        console.log(userId)
+        console.log(serverId)
+        // Check if the user is a member in the server
+        const isMember = await Membership.exists({
+            server: serverId,
+            member: userId,
         });
-        await newAnnouncement.save();
-        res.json({ message: "Announcement created successfully" });
-    } catch (err) {
-        res.status(500).json({ message: err });
+
+        // Check if the user is an admin in the server
+        const isAdmin = await Membership.exists({
+            server: serverId,
+            member: userId,
+            isAdmin: true,
+        });
+
+        console.log(isAdmin)
+        console.log(isMember)
+
+        if (isMember && isAdmin) {
+            // User is a member and an admin, proceed to the next middleware or route
+            try {
+                // bearer token in header
+                const token = req.headers["authorization"].split(" ")[1];
+                const user = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+                const { title, description } = req.body;
+                const newAnnouncement = new Announcement({
+                    title,
+                    description,
+                    createdBy: user.id,
+                    server: req.body.serverId,
+                });
+                await newAnnouncement.save();
+                res.json({ message: "Announcement created successfully" });
+            } catch (err) {
+                res.status(500).json({ message: err });
+            }
+        } 
+        else {
+            // User is not a member or not an admin, return forbidden status
+            return res.status(403).json({ message: "Forbidden, user not authorized" });
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Internal Server Error" });
     }
+            
+
 };
+
+// exports.createAnnouncement = async (req, res) => {
+//     logEndPoint("POST", "/announcements");
+
+//     try {
+//         // Call the checkUserInServerAndAdmin middleware before proceeding
+//         //const x = await checkUserInServerAndAdmin(req, res);
+//         //console.log(x)
+//         // bearer token in header
+//         const token = req.headers["authorization"].split(" ")[1];
+//         const user = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+//         const { title, description } = req.body;
+
+//         // Create a new announcement
+//         const newAnnouncement = new Announcement({
+//             title,
+//             description,
+//             createdBy: user.id,
+//             server: req.params.serverId,
+//         });
+
+//         await newAnnouncement.save();
+//         res.json({ message: "Announcement created successfully" });
+//     } catch (err) {
+//         res.status(500).json({ message: "asa" });
+//     }
+// };
+
 
 // Read
 exports.getAnnouncements = async (req, res) => {
--- backend/src/middleware/middleware.js
+++ backend/src/middleware/middleware.js
@@ -32,4 +32,46 @@ const checkUserRole = (requiredRole) => {
     };
 };
 
+
+//check user role inside gossip
+const { Membership } = require("../models/server.model");
+
+const checkUserInServerAndAdmin = async (req, res, next) => {
+    try {
+        const token = req.headers["authorization"].split(" ")[1];
+        if (!token) {
+            return res.status(401).json({ error: "You must be logged in" });
+        }
+
+        const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        const userId = decoded.id;
+        const serverId = req.params.serverId; // Assuming serverId is in the request parameters
+
+        // Check if the user is a member in the server
+        const isMember = await Membership.exists({
+            server: serverId,
+            member: userId,
+        });
+
+        // Check if the user is an admin in the server
+        const isAdmin = await Membership.exists({
+            server: serverId,
+            member: userId,
+            isAdmin: true,
+        });
+
+        if (isMember && isAdmin) {
+            // User is a member and an admin, proceed to the next middleware or route
+            return res.status(200);
+        } else {
+            // User is not a member or not an admin, return forbidden status
+            return res.status(403).json({ message: "Forbidden, user not authorized" });
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
+};
+
+module.exports = { checkUserInServerAndAdmin };
 module.exports = { checkUserRole };
