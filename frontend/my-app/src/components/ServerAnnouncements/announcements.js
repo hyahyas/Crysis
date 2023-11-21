a4084e32422542cfbe5191bced38ea79e5089e55
Hassan <hassanyahyasheikh@gmail.com>
2023-11-21 15:06:09 +0500

    announcement tickets page with placeholder data, awaiting integration

--- frontend/my-app/src/components/ServerAnnouncements/announcements.js
+++ frontend/my-app/src/components/ServerAnnouncements/announcements.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
 
 const Announcements = () => {
     const [currentPage, setCurrentPage] = useState(1);
-
+    const navigate = useNavigate()
     const itemsPerPage = 5; // pagenation
 
     const placeholderAnnouncements = [
@@ -40,7 +41,6 @@ const Announcements = () => {
             description: "This is a placeholder announcement. Add your content here.",
             createdBy: "Ahsan Ali Khan",
             date: "2023-01-15",
-
         },
         {
             id: 6,
@@ -48,7 +48,6 @@ const Announcements = () => {
             description: "Another placeholder announcement. Customize as needed.",
             createdBy: "hassan",
             date: "2023-01-20",
-
         },
         {
             id: 7,
@@ -56,7 +55,6 @@ const Announcements = () => {
             description: "This is a placeholder announcement. Add your content here.",
             createdBy: "Ahsan Ali Khan",
             date: "2023-01-15",
-            
         }
     ];
 
@@ -65,61 +63,78 @@ const Announcements = () => {
     const currentAnnouncements = placeholderAnnouncements.slice(indexOfFirstItem, indexOfLastItem);
 
     const paginate = (pageNumber) => setCurrentPage(pageNumber);
+    const handleChatClick = () => {
+        navigate("/chat");
+    };
+
+    const handleAnnouncementsClick = () => {
+        // No need to navigate, as the announcements are already on this page
+    };
+
+    const handleTicketsClick = () => {
+        navigate("/tickets");
+    };
 
     return (
-        <div className="grid grid-cols-12 gap-6 p-4">
-            {/* Left column for chat, announcements, and tickets */}
-            <div className="col-span-3 bg-gray-200 p-4">
-                <button
-                    className={`w-full py-2 mb-2 rounded-md text-left ${
-                        currentPage === 2 ? "bg-indigo-500 text-white" : "hover:bg-gray-300"
-                    }`}
-                >
-                    Announcements
-                </button>
-                <button
-                    className={`w-full py-2 mb-2 rounded-md text-left ${
-                        currentPage === 1 ? "bg-indigo-500 text-white" : "hover:bg-gray-300"
-                    }`}
-                >
-                    Chat
-                </button>
-                <button
-                    className={`w-full py-2 mb-2 rounded-md text-left ${
-                        currentPage === 3 ? "bg-indigo-500 text-white" : "hover:bg-gray-300"
-                    }`}
-                >
-                    Tickets
-                </button>
+        <div>
+            {/* Top header (black) */}
+            <div className="bg-black text-white p-4">
+                <h2>{/* Replace with the server name */} Server Announcements</h2>
+                {/* Add your buttons or any other elements here */}
             </div>
 
-            {/* Main content area */}
-            <div className="col-span-9">
-                <h2 className="text-2xl font-bold mb-4">Announcements</h2>
+            {/* Content area */}
+            <div className="grid grid-cols-12 gap-6 p-4">
+                {/* Left column for chat, announcements, and tickets */}
+                <div className="col-span-3 bg-gray-200 p-4">
+                    <button
+                        onClick={handleAnnouncementsClick}
+                        className="w-full py-2 mb-2 rounded-md text-left bg-indigo-500 text-white"
+                    >
+                        Announcements
+                    </button>
+                    <button
+                        onClick={handleChatClick}
+                        className="w-full py-2 mb-2 rounded-md text-left "
+                    >
+                        Chat
+                    </button>
+                    <button
+                        onClick={handleTicketsClick}
+                        className="w-full py-2 mb-2 rounded-md text-left "
+                    >
+                        Tickets
+                    </button>
+                </div>
 
-                {/* Display Announcements */}
-                {currentAnnouncements.map((announcement) => (
-                    <div key={announcement.id} className="mb-8 bg-white p-6 rounded-md shadow-md">
-                        <h3 className="text-xl font-bold mb-2">{announcement.title}</h3>
-                        <p className="text-gray-500 mb-2">{announcement.date}</p>
-                        <p className="text-gray-800">{announcement.description}</p>
-                        <p className="text-gray-500 mt-2">Created by: {announcement.createdBy}</p>
-                    </div>
-                ))}
+                {/* Main content area */}
+                <div className="col-span-9">
+                    <h2 className="text-2xl font-bold mb-4">Announcements</h2>
 
-                {/* Pagination */}
-                <div className="flex justify-center mt-4">
-                    {Array.from({ length: Math.ceil(placeholderAnnouncements.length / itemsPerPage) }, (_, index) => (
-                        <button
-                            key={index}
-                            onClick={() => paginate(index + 1)}
-                            className={`mx-1 px-3 py-2 rounded-md ${
-                                currentPage === index + 1 ? "bg-blue-500 text-white" : "bg-gray-300 text-gray-700"
-                            }`}
-                        >
-                            {index + 1}
-                        </button>
+                    {/* Display Announcements */}
+                    {currentAnnouncements.map((announcement) => (
+                        <div key={announcement.id} className="mb-8 bg-white p-6 rounded-md shadow-md">
+                            <h3 className="text-xl font-bold mb-2">{announcement.title}</h3>
+                            <p className="text-gray-500 mb-2">{announcement.date}</p>
+                            <p className="text-gray-800">{announcement.description}</p>
+                            <p className="text-gray-500 mt-2">Created by: {announcement.createdBy}</p>
+                        </div>
                     ))}
+
+                    {/* Pagination */}
+                    <div className="flex justify-center mt-4">
+                        {Array.from({ length: Math.ceil(placeholderAnnouncements.length / itemsPerPage) }, (_, index) => (
+                            <button
+                                key={index}
+                                onClick={() => paginate(index + 1)}
+                                className={`mx-1 px-3 py-2 rounded-md ${
+                                    currentPage === index + 1 ? "bg-blue-500 text-white" : "bg-gray-300 text-gray-700"
+                                }`}
+                            >
+                                {index + 1}
+                            </button>
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
